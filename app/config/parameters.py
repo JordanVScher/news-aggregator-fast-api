58b44383ec96a6d281239486009b5a01255fa054
9902319 <9902319@prestadorserv.com.br>
2025-03-12 16:48:49 -0300

    feat: add check for news api key

--- app/config/parameters.py
+++ app/config/parameters.py
@@ -1,6 +1,11 @@
+from sys import exit
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-PYTHON_ENV = os.getenv("PYTHON_ENV")
+PYTHON_ENV = os.getenv("PYTHON_ENV") or "DEV"
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+
+if not NEWS_API_KEY:
+    exit("Fatal Error: NEWS_API_KEY must be supplied")
